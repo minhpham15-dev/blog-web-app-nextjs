6c808d05aa1943de5a6347e726e05acae72c7b08
minhpham15-dev <minhmatmo158@gmail.com>
2023-11-24 21:43:32 +0700

    add dark theme, responsive menu

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,16 +1,29 @@
 "use client";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-  if (typeof window !== undefined) {
+  if (typeof window !== "undefined") {
     const value = localStorage.getItem("theme");
     return value || "light";
   }
 };
 
 export const ThemeContextProvider = ({ children }) => {
-  const [theme, setTheme] = useState(() => getFromLocalStorage());
-  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+  const [theme, setTheme] = useState(() => {
+    return getFromLocalStorage();
+  });
+  const toggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+  return (
+    <ThemeContext.Provider value={{ theme, toggle }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
